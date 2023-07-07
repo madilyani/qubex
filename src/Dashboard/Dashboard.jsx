8a13a63005d1055b19071deb5f263266de5352d1
madilyani <madilyani1@gmail.com>
2023-07-08 02:06:54 +0500

    asf

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import Nodes from "./components/Nodes";
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
-  )
+    <div className="dash">
+      <div className="auto__container">
+        <div className="dash__inner">
+          <h1>Dashboard</h1>
+          <div className="dash__row row-3">
+            <div className="dash__col">
+              <Nodes />
+            </div>
+            <div className="dash__col">
+              <Nodes />
+            </div>
+            <div className="dash__col">
+              <Nodes />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
