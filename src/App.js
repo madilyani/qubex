c1c533229eb0a0b2dec6fae1e700942a4e0b4f46
madilyani <madilyani1@gmail.com>
2023-07-08 01:05:39 +0500

    adsg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Dashboard from "./Dashboard/Dashboard";
 
-function App() {
+export default function App() {
+  const [isAuth, setIsAuth] = useState(true);
+  const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
+  window.addEventListener("resize", appHeight);
+  appHeight();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="" element={<Dashboard setIsAuth={setIsAuth} />} />
+      </Routes>
+    </>
   );
 }
-
-export default App;
---
+++ src/Workload/Workload.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Workload() {
+  return (
+    <div>Workload</div>
+  )
+}
