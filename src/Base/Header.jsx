d68def4dd2f1f597215f771a21ab168167ed7885
madilyani <madilyani1@gmail.com>
2023-07-08 02:01:14 +0500

    asdg

--- src/Base/Header.jsx
+++ src/Base/Header.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { accountIcon, bellIcon } from "./SVG";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="header">
+      <div className="auto__container">
+        <div className="header__inner">
+          <div className="nav">
+            <div className="nav__inner">
+              <Link to="/" className="nav__inner-link">
+                Documentation
+              </Link>
+              <Link to="/" className="nav__inner-link">
+                API
+              </Link>
+              <Link to="/" className="nav__inner-link">
+                Help
+              </Link>
+              <Link to="/" className="nav__inner-link">
+                {accountIcon} AlonYariv
+              </Link>
+              <div className="nav__inner-btns">
+                <button type="button" className="nav__inner-not">
+                  {bellIcon}
+                </button>
+                <div className="nav__inner-avatar">
+                  <img
+                    src={process.env.PUBLIC_URL + "/images/avatar.png"}
+                    alt="avatar"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Base/Sidebar.jsx
+++ src/Base/Sidebar.jsx
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  chevronBottom,
+  clusterIcon,
+  containerIcon,
+  dashboardIcon,
+  nodeIcon,
+} from "./SVG";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Sidebar() {
+  const [isShow, setIsShow] = useState(false);
   return (
     <div className="sidebar">
-      <div className="sidebar__inner"></div>
+      <div className="sidebar__inner">
+        <div className="sidebar__head">
+          <div className="sidebar__head-logo">
+            <img src={process.env.PUBLIC_URL + "/images/logo.svg"} alt="logo" />
+          </div>
+          <Link to="" className="sidebar__link">
+            {clusterIcon} <span>All clusters</span>
+          </Link>
+        </div>
+        <div className="sidebar__body">
+          <NavLink to="" className="sidebar__link">
+            {dashboardIcon} <span>Dashboard</span>
+          </NavLink>
+          <div to="" className={"sidebar__drop " + (isShow ? "active" : "")}>
+            <div
+              className="sidebar__drop-btn"
+              onClick={() => setIsShow(!isShow)}
+            >
+              {containerIcon}
+              <span>
+                Container size <br />
+                monitoring
+              </span>
+              <i>{chevronBottom}</i>
+            </div>
+            {isShow && (
+              <div className="sidebar__drop-content">
+                <Link to="" className="sidebar__link">
+                  {nodeIcon} <span>Node list</span>
+                </Link>
+                <Link to="" className="sidebar__link">
+                  {nodeIcon} <span>Node list</span>
+                </Link>
+              </div>
+            )}
+          </div>
+          <Link to="" className="sidebar__link">
+            {nodeIcon} <span>Node list</span>
+          </Link>
+          <Link to="" className="sidebar__link">
+            {nodeIcon}{" "}
+            <span>
+              Workload rightsizing <br />
+              Reaction time
+            </span>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
